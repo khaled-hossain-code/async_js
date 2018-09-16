e08043db49c168a915c6f576e3f5dbec7608c4ee
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-09-16 08:50:58 +0600

    async await

---
+++ solution.js
@@ -0,0 +1,49 @@
+async function mailMovies() {
+  try {
+    const customer = await getCustomer(1);
+    console.log('Customer: ', customer);
+    let movies;
+
+    if (customer.isGold) {
+      movies = await getTopMovies(); 
+      console.log('Top movies: ', movies);
+    }
+
+    await sendEmail(customer.email, movies);
+    console.log('Email sent...')
+  }catch(err) {
+    console.log(err.message);
+  }
+}
+
+mailMovies();
+
+function getCustomer(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: 1,
+        name: "Mosh Hamedani",
+        isGold: true,
+        email: "email"
+      });
+    }, 4000);
+  });
+}
+
+function getTopMovies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 4000);
+  });
+}
+
+function sendEmail(email, movies, callback) {
+  return new Promise((resolve, reject) => {    
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
+}
+
